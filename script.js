b89e1b5cd33de73cbb63244b50dbdfbef64c977c
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2024-01-12 16:49:25 +0330

    chore: fixed animations on opne and close

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const modal = document.querySelector('.modal-container');
+const modalContainer = document.querySelector('.modal-container');
 const openModal = document.querySelector('#openbtn');
 const closeModal = document.querySelector('.closebtn');
 
-
 //زمانی ک کاربر بر روی دکمه اپن میزند دیسپلی از نان ب  بلاک تبدیل میشود
 openModal.addEventListener('click', () => {
-    // modal.classList.remove('closeani');
-    modal.style.display = "block";
-})
-
-
+  modalContainer.classList.add('show');
+  modalContainer.classList.remove('closeModalAnimationClass');
+});
 
 closeModal.addEventListener('click', () => {
-    modal.classList.add('closeani');
-    modal.style.display = "none";
-})
-
+  modalContainer.classList.add('closeModalAnimationClass');
+  modalContainer.classList.remove('show');
+});
 
 window.addEventListener('click', (event) => {
-    if(event.target == modal){
-        modal.classList.add('closeani');
-        modal.style.display ='none';
-
-    } 
-
-})
+  if (event.target == modal) {
+    // modalContainer.style.display = 'none';
+    modalContainer.classList.remove('show');
+  }
+});
